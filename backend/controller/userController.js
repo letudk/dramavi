5ef29f55a465e0e7515f9127c43bbed0a5392212
letudk <tule.itc@gmail.com>
2022-12-29 16:18:41 +0700

    update controller user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -22,7 +22,6 @@ export const signup = async(req, res, next) => {
     }catch(err){
         return console.log(err);
     }
-
     if(checkAvailedUser){
         return res.status(400).json({message:"Nguoi dung da ton tai"});
     }
@@ -48,7 +47,6 @@ export const login = async(req,res,next) => {
     }catch(err){
         return console.log(err);
     }
-
     if(!checkAvailedUser){
         return res.status(404).json({message:"Nguoi dung khong ton tai"});
     }
@@ -61,52 +59,52 @@ export const login = async(req,res,next) => {
 }
 
 export const deleteUser = async(req, res, next) => {
-    const {email} = req.body;
-    let checkAvailedUser;
-    let checkDelete;
-    try{
-        checkAvailedUser = await users.findOne({email});
-    }catch(err){
-        return console.log(err);
+    const userId = req.params.id;
+    let user;
+    try {
+        user = await users.findByIdAndRemove(userId);
+    } catch (error) {
+        return console.log(error);
     }
-    if(!checkAvailedUser){
-        return res.status(404).json({message:"Nguoi dung khong ton tai"});
+    if(!user) {
+        return res.status(500).json({message:"Khong the xoa user"});
     }
-
-    checkDelete = await users.deleteOne({email});
-
-    if(!checkDelete){
-        return res.status(404).json({message:"Xoa that bai"});
-    }
-    return res.status(201).json({message:"Xoa thanh cong"});
-
+    return res.status(200).json({message:"Xoa user thanh cong"});
 }
 
 export const updateUser = async(req, res, next) =>{
     const {email,avatar,name,status, password} = req.body;
-    let checkAvailedUser;
-    // kiem tra nguoi dung co ton tai hay khong truoc khi update
-    try{
-        checkAvailedUser = await users.findOne({email});
-    }catch(err){
-        return console.log(err);
+
+    const userID = req.params.id;
+    let user;
+     
+    try {
+        user = await users.findByIdAndUpdate(userID, {
+            avatar,
+            name,
+            status,
+            password
+        })
+    } catch (error) {
+        return console.log(error);
     }
-    if(!checkAvailedUser){
-        return res.status(404).json({message:"Nguoi dung khong ton tai"});
-    }
-    const hashPassword = bcrypt.hashSync(password);
-    const docUpdate = {
-        $set:{
-            name: name,
-            avatar: avatar,
-            status:status,
-            password: hashPassword
-        }
+    if(!user) {
+        return res.status(500).json({message:"Khong the cap nhat"});
     }
 
-    const result = await users.updateOne({email}, docUpdate);
-    if(!result){
-        return res.status(404).json({message:"Update that bai"});
+    return res.status(201).json({user});
+}
+
+export const getUserById = async(req,res,next) => {
+    const userId = req.params.id;
+    let user;
+    try {
+        user = await users.findById(userId);
+    } catch (error) {
+       return console.log(error);
     }
-    return res.status(201).json({users});
+    if(!user){
+        return res.status(404).json({message:"Nguoi dung khong ton tai"});
+    }
+    return res.status(201).json({user});
 }
--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getAllUser, signup, login, deleteUser,updateUser } from "../controller/userController.js";
+import { getAllUser, signup, login, deleteUser,updateUser,getUserById } from "../controller/userController.js";
 const routes = express.Router();
 
 routes.get('/', getAllUser);
+routes.get('/:id', getUserById);
 routes.post('/signup', signup);
 routes.post('/login', login);
-routes.post('/delete', deleteUser);
-routes.post('/update', updateUser);
+routes.delete('/:id', deleteUser);
+routes.put('/update/:id', updateUser);
 
 export default routes;
